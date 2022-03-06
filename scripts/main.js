1b54cf14a935d654ebac8d5a60c5a457d2bfef01
Cole Elbing <8517401@isd535.org>
2022-03-06 16:43:41 -0600

    Had to retrace my steps to create a event listener for when you click on a topic, however it is not working and my brains isn't working so i'm going to take a break.

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,36 @@
-// Put your JavaScript here
+const topics = ['Crainial', 'Midbrain', 'Pons', 'Medula']
+const CHANGE_THE_ID = '[data-text-role ="target"]';
+const TOPIC_TYPE_SELECTOR = '[data-text-role = "trigger"]';
+
+function setChanges(textId){
+    'use strict';
+    let differentText = document.querySelector(CHANGE_THE_ID);
+    differentText.setAttribute('id' , textId)
+    document.getElementById(textId).innerHTML = 'hello';
+
+}
+
+function idFromText(paragraph){
+    'use strict';
+    return paragraph.getAttribute('id')
+}
+
+function setIdChanges(paragraph){
+    'use strict';
+    setChanges(idFromText(paragraph));
+}
+
+function addTopicClickHandler(topic){
+    'use strict';
+    topic.addEventListener('click', function(event){
+        event.preventDefault();
+        setIdChanges(paragraph);
+    });
+}
+
+function initializeEvents(){
+    'use strict';
+    topics.forEach(addTopicClickHandler);
+}
+
+initializeEvents();
